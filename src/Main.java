fb832a77ca81d7da1bc2e0a849dd0fc4c5d85871
Joseph-ui-sketch <hernajos000@warren.k12.in.us>
2024-09-18 13:07:51 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+    }
+
+    private static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+        while (number > 0);
+
+        getEvenDigitSum(10);
+
+
+
+        if (  == %2);
+
+
+                number /= 10;
+
+        return sum;
+
+    }
+}
